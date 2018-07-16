03ecc2a29d856429d12e3be902bb117bb2d596de
Sait Cakmak <saitcakmak@outlook.com>
2018-07-16 15:41:47 -0400

    value map fixed

--- value_map.py
+++ value_map.py
@@ -11,9 +11,9 @@ prob_str = "prod"
 mu_gamma = 1
 std_gamma = 0.1
 alpha = 0.9
-rep, n0, m0 = 40, 100, 10
+rep0, n0, m0 = 10, 100, 100
 t0 = 5
-length, precision = 150, 10
+length0, precision0 = 50, 10
 
 
 def collect_inner_samples(m, gamma, theta):
@@ -45,19 +45,38 @@ def collect_samples(n, m, theta):
     return sample_list, derivative_list
 
 
-def sample():
+def calc_der(n, m, theta):
+    sample_list, derivative_list = collect_samples(n, m, theta)
+
+    var_alpha = np.sort(sample_list)[int(n * alpha)]
+
+    cvar_list = []
+    cvar_der_list = []
+    for i in range(n):
+        if sample_list[i] >= var_alpha:
+            cvar_list.append(sample_list[i])
+            cvar_der_list.append(derivative_list[i])
+
+    return np.average(cvar_list), np.average(cvar_der_list)
+
+
+def sample(n=n0, m=m0, rep=rep0, length=length0, precision=precision0, t=t0):
     begin = datetime.datetime.now()
-    val_list =[]
+    val_list = []
+    der_list = []
     for i in range(length):
         in_list = []
+        in_der_list = []
         for j in range(rep):
             now = datetime.datetime.now()
             print("i: ", i, " j: ", j, " time: ", now - begin)
-            samp, der = collect_samples(n0, m0, (t0 + i/precision))
+            samp, der = calc_der(n, m, (t + i/precision))
             in_list.append(samp)
+            in_der_list.append(der)
         val_list.append(np.average(in_list))
-    return val_list
-
-
-vals = sample()
-np.save(prob_str + "_vals_mu" + str(mu_gamma) + "_std" + str(std_gamma) + "_" + str(t0) + "to" + str(t0 + length/precision) + "_" + str(rep) + "x" + str(n0) + "x" + str(m0), vals)
+        der_list.append(np.average(in_der_list))
+    np.save(prob_str + "_fixed_vals_mu" + str(mu_gamma) + "_std" + str(std_gamma) + "_" + str(t) + "to" + str(
+        t0 + length / precision) + "_" + str(rep) + "x" + str(n) + "x" + str(m), val_list)
+    np.save(prob_str + "_fixed_der_mu" + str(mu_gamma) + "_std" + str(std_gamma) + "_" + str(t) + "to" + str(
+        t0 + length / precision) + "_" + str(rep) + "x" + str(n) + "x" + str(m), der_list)
+    return val_list, der_list
