bd4d332c3b3531334557f4ec64d0e06208914bec
Sait Cakmak <saitcakmak@outlook.com>
2018-07-17 17:36:06 -0400

    prob update

--- investment.py
+++ investment.py
@@ -1,8 +1,8 @@
 """
 this is the investment problem as described in my notes with 3 assets for now
-returns are normal with mean base + gamma * beta and std dev 0.3 * gamma * beta
+returns are normal with mean base + gamma * beta and std dev 0.3 * beta
 the cost of capital is c1 * cap + c2 * cap^2
-v2 has higher variance for higher beta, i.e, std = 0.4 * gamma * beta^2
+v2 has higher variance for higher beta, i.e, std = 0.4 * beta^2
 v3 has even higher variance
 """
 import numpy as np
@@ -16,7 +16,7 @@ base = 0.04
 def invest(gamma, theta, seed=0):
     if seed:
         np.random.seed(seed)
-    x = np.random.multivariate_normal(0, np.diag(b)) * 0.3 * gamma + base + gamma * b
+    x = np.random.multivariate_normal(0, np.diag(b)) * 0.3 + base + gamma * b
     cap = sum(theta)
     val = c1 * cap + c2 * cap ** 2 - np.dot(theta, x)
     der = c1 + 2 * c2 * cap - x
@@ -26,7 +26,7 @@ def invest(gamma, theta, seed=0):
 def investv2(gamma, theta, seed=0):
     if seed:
         np.random.seed(seed)
-    x = np.random.multivariate_normal(0, np.diag(np.square(b))) * 0.4 * gamma + base + gamma * b
+    x = np.random.multivariate_normal(0, np.diag(np.square(b))) * 0.4 + base + gamma * b
     cap = sum(theta)
     val = c1 * cap + c2 * cap ** 2 - np.dot(theta, x)
     der = c1 + 2 * c2 * cap - x
@@ -36,7 +36,7 @@ def investv2(gamma, theta, seed=0):
 def investv3(gamma, theta, seed=0):
     if seed:
         np.random.seed(seed)
-    x = np.random.multivariate_normal(0, np.diag(np.power(b, 3))) * 0.5 * gamma + base + gamma * b
+    x = np.random.multivariate_normal(0, np.diag(np.power(b, 3))) * 0.5 + base + gamma * b
     cap = sum(theta)
     val = c1 * cap + c2 * cap ** 2 - np.dot(theta, x)
     der = c1 + 2 * c2 * cap - x
