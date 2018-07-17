751dc217cb70f917aafd9fc3058c0f4ae1c40d16
Sait Cakmak <saitcakmak@outlook.com>
2018-07-17 17:12:29 -0400

    prob update

--- investment.py
+++ investment.py
@@ -2,7 +2,7 @@
 this is the investment problem as described in my notes with 3 assets for now
 returns are normal with mean gamma * beta and std dev 0.3 * gamma * beta
 the cost of capital is c1 * cap + c2 * cap^2
-v2 has higher variance for higher beta, i.e, std = 0.3 * gamma * beta^2
+v2 has higher variance for higher beta, i.e, std = 0.4 * gamma * beta^2
 """
 import numpy as np
 
@@ -24,7 +24,7 @@ def invest(gamma, theta, seed=0):
 def investv2(gamma, theta, seed=0):
     if seed:
         np.random.seed(seed)
-    x = np.random.multivariate_normal(gamma * b, 0.3 * gamma * np.diag(np.square(b)))
+    x = np.random.multivariate_normal(gamma * b, 0.4 * gamma * np.diag(np.square(b)))
     cap = sum(theta)
     val = c1 * cap + c2 * cap ** 2 - np.dot(theta, x)
     der = c1 + 2 * c2 * cap - x
