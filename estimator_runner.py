6da701add3d45956feeabc8663571097cfcf4d10
Sait Cakmak <saitcakmak@outlook.com>
2019-05-28 19:33:20 -0400

    exp clear

--- estimator_runner.py
+++ estimator_runner.py
@@ -47,7 +47,7 @@ def simple_run(estimator, budget, rep, alpha, rho):
             results[i] = estimator(theta, x, budget, alpha, rho)
     else:
         return 0
-    np.savetxt("run_save_"+rho+"_"+estimator_text+"_budget_"+str(budget)+"_rep_"+str(rep)+"_time_"+str(datetime.datetime.now())+".csv",
+    np.savetxt(rho+"_"+estimator_text+"_budget_"+str(budget)+"_rep_"+str(rep)+"_time_"+str(datetime.datetime.now())+".csv",
                X=results, delimiter=";")
 
     return results
